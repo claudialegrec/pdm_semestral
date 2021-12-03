304225e8c8549692f03d664ba8344cec617ab0ed
Alfredo Garcia <60104086+AlfredoGarciaYapor@users.noreply.github.com>
2021-12-03 14:34:05 -0700

    se agregaron cosas para leer el gist

--- app/src/main/java/com/example/semestral/HomeActivity.kt
+++ app/src/main/java/com/example/semestral/HomeActivity.kt
@@ -1,11 +1,44 @@
 package com.example.semestral
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var Listweb: ListView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        Listweb = findViewById(R.id.listViewWebService)
+        var url = "https://gist.githubusercontent.com/AlfredoGarciaYapor/34113c8ffd86610f1a08e4d5eb4ebb6f/raw/499d79be631dc0db26bcd629668140830dfa2d36/business.json"
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(
+            Request.Method.GET, url, Response.Listener { response ->
+
+                Log.d("response","La respuesta es ${response}")
+
+                val peoplelist = Gson().fromJson(response, PeopleInfo::class.java).people
+
+                Log.d("Name", "${peoplelist[0].name}")
+
+                val adapter = peopleAdpter(this, peoplelist)
+
+                Listweb.adapter = adapter
+
+            },
+            Response.ErrorListener {
+
+            })
+        queue.add(stringRequest)
     }
 }
